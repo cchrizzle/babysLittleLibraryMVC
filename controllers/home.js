e9a140529d7b80b7374a385b9860500c5a32ab70
Christopher Chang <chris.chang.dev@proton.me>
2025-03-25 16:50:10 -0700

    Added bookCount to show total books added.

--- controllers/home.js
+++ controllers/home.js
@@ -2,11 +2,11 @@ const Book = require('../models/Book');
 
 module.exports = {
     getIndex: async (req, res) => {
-        // const bookList = await Book.find();
+        const bookCount = await Book.find();
         const finishedBooks = await Book.find({ finished: true });
         const finishedBooksCount = await Book.countDocuments({ finished: true });
         const unreadBooks = await Book.find({ finished: false });
         const unreadBooksCount = await Book.countDocuments({ finished: false });
-        res.render('index.ejs', { finished: finishedBooks, finishedCount: finishedBooksCount, unread: unreadBooks, unreadCount: unreadBooksCount });
+        res.render('index.ejs', { bookCount, finished: finishedBooks, finishedCount: finishedBooksCount, unread: unreadBooks, unreadCount: unreadBooksCount });
     },
 };
