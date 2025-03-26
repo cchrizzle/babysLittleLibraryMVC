2de93b39fca522f2590a0d920d92b925553fc461
Christopher Chang <chris.chang.dev@proton.me>
2025-03-26 12:36:07 -0700

    Changed "markComplete" route to "markFinished".

--- routes/books.js
+++ routes/books.js
@@ -6,13 +6,11 @@ const booksController = require('../controllers/books');
 
 router.post('/addBook', booksController.addBook);
 
-// Adding below following Leon's setup, but need to process and see.
-
-router.put('/markComplete/:id', booksController.markComplete);
+router.put('/markFinished/:id', booksController.markFinished);
 
 router.put('/markToRead/:id', booksController.markToRead);
 
-router.put('markFave/:id', booksController.markFave);
+router.put('/markFave/:id', booksController.markFave);
 
 router.delete('/deleteBook/:id', booksController.deleteBook);
 
