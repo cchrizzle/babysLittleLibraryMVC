796a4d1b2e6e348252e290c9fa4b94dcf722c3cf
Christopher Chang <chris.chang.dev@proton.me>
2025-03-31 20:30:51 -0700

    Deleted other routes.

--- routes/food.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const foodController = require('../controllers/food');
-
-router.get('/', foodController.getFood);
-
-module.exports = router;
--- routes/toys.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const toysController = require('../controllers/toys.js');
-
-router.get('/', toysController.getToys);
-
-router.post('/addToy', toysController.addToy);
-
-module.exports = router;
