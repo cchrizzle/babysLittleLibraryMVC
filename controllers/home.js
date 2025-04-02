823de804e8f29e038796aafdbdd10e8c27d89486
Christopher Chang <chris.chang.dev@proton.me>
2025-04-01 18:07:17 -0700

    Added authentication so now adding user parameter to link entries.

--- controllers/home.js
+++ controllers/home.js
@@ -1,12 +1,17 @@
 const Book = require('../models/Book');
 
 module.exports = {
+    getLogin: (req, res) => {
+        if (!req.user) {
+            return res.render('login');
+        }
+    },
     getIndex: async (req, res) => {
-        const bookCount = await Book.find();
-        const finishedBooks = await Book.find({ finished: true });
-        const finishedBooksCount = await Book.countDocuments({ finished: true });
-        const unreadBooks = await Book.find({ finished: false });
-        const unreadBooksCount = await Book.countDocuments({ finished: false });
+        const bookCount = await Book.countDocuments({ user: req.user._id });
+        const finishedBooks = await Book.find({ user: req.user.id, finished: true });
+        const finishedBooksCount = await Book.countDocuments({ user: req.user.id, finished: true });
+        const unreadBooks = await Book.find({ user: req.user.id, finished: false });
+        const unreadBooksCount = await Book.countDocuments({ user: req.user.id, finished: false });
         res.render('index.ejs', { bookCount, finished: finishedBooks, finishedCount: finishedBooksCount, unread: unreadBooks, unreadCount: unreadBooksCount });
     },
 };
