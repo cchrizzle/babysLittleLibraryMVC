9c707c374decfe4b6a35ba7e040d2b91dc8f1db9
Christopher Chang <chris.chang.dev@proton.me>
2025-04-01 18:06:17 -0700

    Trying to implement stats on read books - original date finished, number of times read, etc.

--- controllers/books.js
+++ controllers/books.js
@@ -1,32 +1,33 @@
 const Book = require('../models/Book');
 
 module.exports = {
-    // Don't need getBooks controller since it's the same thing as home.getIndex
-    getBooks: async (req, res) => {
-        try {
-            const bookCount = await Book.find();
-            const finishedBooks = await Book.find({ finished: true });
-            const finishedBooksCount = await Book.countDocuments({ finished: true });
-            const unreadBooks = await Book.find({ finished: false });
-            const unreadBooksCount = await Book.countDocuments({ finished: false });
-            res.render('index.ejs', { bookCount, finished: finishedBooks, finishedCount: finishedBooksCount, unread: unreadBooks, unreadCount: unreadBooksCount });
-        } catch (err) {
-            console.error(err);
-        }
-    },
     addBook: async (req, res) => {
         try {
             const { bookTitle, authorFirstName, authorLastName, finished, favorite } = req.body;
             if (!bookTitle || !authorFirstName) {
                 return res.status(400).json({ error: 'Title and first name required!' });
             }
-            const newBook = await Book.create({
+            const bookData = {
                 bookTitle,
                 authorFirstName,
                 authorLastName,
                 finished: finished === 'true', // To enforce boolean
                 favorite,
-            });
+                user: req.user._id,
+            };
+
+            // Only add dateFinished if book finished and date provided
+            if (finished === 'true') {
+                const { dateFinished } = req.body;
+                if (finished === true) {
+                    const finishedDate = dateFinished ? new Date(dateFinished) : new Date();
+                    bookData.dateFinished = finishedDate;
+                    bookData.readCount = 1;
+                    bookData.readHistory = [{ dateFinished: finishedDate }];
+                }
+            }
+
+            const newBook = await Book.create(bookData);
 
             // 3/24/25: Commenting out to reload page, but keeping for future reference: res.status(201).json({ message: 'Book added successfully!', book: newBook });
             console.log(`Book added successfully! "${newBook.bookTitle}" by ${newBook.authorFirstName} ${authorLastName}`);
@@ -38,7 +39,25 @@ module.exports = {
     },
     markFinished: async (req, res) => {
         try {
-            const finishedBook = await Book.findOneAndUpdate({ _id: req.params.id }, { finished: true }, { new: true });
+            const currentDate = new Date();
+
+            // Find book
+            const book = await Book.findById(req.params.id);
+            if (!book) {
+                return res.status(404).send('Book not found.');
+            }
+
+            // Increment read count and update history
+            const finishedBook = await Book.findOneAndUpdate(
+                { _id: req.params.id },
+                {
+                    finished: true,
+                    dateFinished: currentDate,
+                    $inc: { readCount: 1 }, // Increment read count
+                    $push: { readHistory: { dateFinished: currentDate } }, // Add to history
+                },
+                { new: true }
+            );
             console.log(`Finished "${finishedBook.bookTitle}", good job!`);
             res.redirect('/');
         } catch (err) {
@@ -48,7 +67,7 @@ module.exports = {
     markToRead: async (req, res) => {
         try {
             const toReadBook = await Book.findOneAndUpdate({ _id: req.params.id }, { finished: false }, { new: true });
-            console.log(`Added ${toReadBook.bookTitle} to reading list!`);
+            console.log(`Added ${toReadBook.bookTitle} to reading list! You've read this book ${toReadBook.readCount} times!`);
             res.redirect('/');
         } catch (err) {
             console.error(`Error marking as read: ${err}`);
@@ -76,6 +95,36 @@ module.exports = {
             res.redirect('/');
         } catch (err) {
             console.error(`Error deleting book: ${err}`);
+        }
+    },
+    getReadingStats: async (req, res) => {
+        try {
+            // Get most read books
+            const mostReadBooks = await Book.find({ user: req.user._id }).sort({ readCount: -1 }).limit(10);
+
+            // Get monthly reading counts
+            const monthlyReadingCounts = await Book.aggregate([
+                { $match: { user: req.user._id } },
+                { $unwind: '$readHistory' },
+                {
+                    $group: {
+                        _id: {
+                            month: { $month: '$readHistory.dateFinished' },
+                            year: { $year: '$readHistory.dateFinished' },
+                        },
+                        count: { $sum: 1 },
+                    },
+                },
+                { $sort: { '_id.year': 1, '_id.month': 1 } },
+            ]);
+            res.render('stats', {
+                mostReadBooks,
+                monthlyReadingCounts,
+                user: req.user,
+            });
+        } catch (err) {
+            console.error(`Error getting reading stats: ${err}`);
+            res.status(500).send('Error retrieving reading statistics.');
         }
     },
 };
